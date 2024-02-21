4291a82541cbcd9da94fbf97ae080d19c4256d04
brunoreis-dev <b.macedoreis@icloud.com>
2024-02-21 19:11:20 -0300

    create curriculum app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,127 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.css";
+import person from "./person";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className={styles.container}>
+      <h1 className={styles.title}>{person.name}</h1>
+      <p className={styles.role}>{person.role}</p>
+      <div className={styles.contacts}>
+        <p className={styles.contact}>
+          Email:{" "}
+          <a href={`mailto:${person.email}`} className={styles.contactLink}>
+            {person.email}
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p className={styles.contact}>
+          Telefone:{" "}
+          <a href={`tel:${person.phone}`} className={styles.contactLink}>
+            {person.phone}
+          </a>
+        </p>
+        <p className={styles.contact}>
+          Github:{" "}
+          <a href={person.githubUrl} className={styles.contactLink}>
+            {person.github}
+          </a>
+        </p>
+        <p className={styles.contact}>
+          Linkedin:{" "}
+          <a href={person.linkedin} className={styles.contactLink}>
+            {person.linkedin}
+          </a>
+        </p>
+        <p className={styles.contact}>
+          Idade: <span className={styles.contactInfo}>{person.age}</span>
+        </p>
+        <p className={styles.contact}>
+          Endereço:{" "}
+          <span className={styles.contactInfo}>{person.location}</span>
+        </p>
+      </div>
+      <div className={styles.box}>
+        <div>
+          <h2 className={styles.subtitles}>Tecnologias Principais</h2>
+          {person.technologies.map((tech) => (
+            <p key={tech.name} className={styles.tech}>
+              {tech.name} - {tech.experience}
+            </p>
+          ))}
+        </div>
+        <div>
+          <h2 className={styles.subtitles}>Educação</h2>
+          {person.education.map((item) => (
+            <div key={item.name}>
+              <p className={styles.educationUniversity}>{item.university}</p>
+              <p className={styles.educationName}>{item.name}</p>
+              <p>{item.time}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className={styles.experience}>
+        <div className={styles.experienceTitle}>
+          <h2 className={styles.subtitles}>Experiência</h2>
+          <p className={styles.experienceTotalTime}>
+            ({person.totalExperience})
+          </p>
+        </div>
+        <div className={styles.experiences}>
+          {person.experiences.map((experience) => (
+            <div key={experience.id}>
+              <div className={styles.experienceBox}>
+                <p className={styles.experienceName}>{experience.name}</p>
+                <p className={styles.experienceType}>
+                  {experience.contractType}
+                </p>
+              </div>
+              <div className={styles.experienceBox}>
+                <p className={styles.experienceRole}>{experience.role}</p>
+                <p className={styles.experienceTime}>{experience.time}</p>
+              </div>
+
+              {experience.hardSkills.length > 0 && (
+                <>
+                  <p className={styles.experienceSkillTitle}>Desenvolvido</p>
+                  <div className={styles.skillLits}>
+                    {experience.hardSkills.map((skill) => (
+                      <p key={skill} className={styles.skill}>
+                        {skill}
+                      </p>
+                    ))}
+                  </div>
+                </>
+              )}
+
+              {experience.softSkills.length > 0 && (
+                <>
+                  <p className={styles.experienceSkillTitle}>Soft Skills</p>
+                  <div className={styles.skillLits}>
+                    {experience.softSkills.map((skill) => (
+                      <p key={skill} className={styles.skill}>
+                        {skill}
+                      </p>
+                    ))}
+                  </div>
+                </>
+              )}
+
+              {experience.references.length > 0 && (
+                <>
+                  <p className={styles.experienceSkillTitle}>Referências</p>
+                  <div className={styles.skillLits}>
+                    {experience.references.map((reference) => (
+                      <p key={reference} className={styles.reference}>
+                        {reference}
+                      </p>
+                    ))}
+                  </div>
+                </>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
